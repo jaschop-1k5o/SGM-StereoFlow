baae4077e7eacd603cea975a420af9a7f3987c46
ga25yiw <ga25yiw@mytum.de>
2017-07-30 15:18:41 +0200

    edited scripts

---
+++ tools/data_setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#USAGE: place in {KITTI Benchmark results directory}/data
+#PARAMETERS: start & end KITTI image nr & SGM outputs folder, e.g. "bash eval.sh 0 20 ../SGM_StereoFlow/outputs"
+#WHAT IT DOES: copies&renames stereoflow images from outputs folder ($3) into . for evaluation by KITTI Benchmark
+
+for imgId in $(seq -f "%06g" $1 $2)
+do
+  cp {$3}/{$imgId}/disparityStereoFlow.png {$imgId}_10.png
+done
